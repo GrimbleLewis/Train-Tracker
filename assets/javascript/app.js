25404f93f17f1ced67855e8fea280c3e7bce3f15
Grimble <grimblelewisthethird@gmail.com>
2019-03-28 16:29:26 -0400

    added the ability to push info from the firebase onto the train schedule but havent been able to correctly produce the next arrival time and minutes away value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,36 +12,90 @@ var config = {
   
   var database = firebase.database();
 
-
-
+ 
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
 
 
     var trainName = $("#train-name-input").val().trim();
     var destination = $("#destination-input").val().trim();
-    var trainTime = $("#first-train-time-input").val().trim();
+    var trainTime = moment($("#first-train-time-input").val().trim(), "HH:mm").format('HH:mm');
     var frequency = $("#frequency-input").val().trim();
 
-    database.ref().push({
-        trainName,
-        destination,
-        trainTime,
-        frequency
-    });
-  
+    var newTrain = {
+       name: trainName,
+       destination: destination,
+       time: trainTime,
+       frequency: frequency
+    };
 
+    database.ref().push(newTrain);
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-time-input").val("");
+    $("#frequency-input").val("");
   });
 
   
-  
+ 
 
   database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
 
+    
+
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var frequency = childSnapshot.val().frequency;
 
 
+    var currentTime = moment();
 
+    var nextTrain;
+  
+    // var trainTimePretty = moment.unix(trainTime).format("X")
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+
+
+    console.log(currentTime)
+
+    var trainTimePretty = moment(trainTime, "HH:mm")
+    console.log(trainTimePretty)
+
+  
+    if (trainTime > currentTime) {
+        nextTrain = trainTime;
+        minutesTilNextTrain = remainder;
+    } else {
+        var minutesPast = currentTime.diff(trainTimePretty, 'minutes');
+        var remainder = minutesPast % frequency;
+        var minutesTilNextTrain = frequency - remainder;
+        nextTrain = currentTime.add(minutesTilNextTrain, 'minutes');
+    }
+    console.log(minutesPast)
+
+    console.log(frequency)
+
+    console.log(remainder)
+
+    console.log(minutesTilNextTrain)
+
+    console.log(minutesPast)
+    
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextTrain.format('HH:mm')),
+        $("<td>").text(minutesTilNextTrain)
+    );
+
+$("#train-table > tbody").append(newRow);
 
 }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
